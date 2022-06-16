468868b7ce786ad159203df0681e8133301da1e9
xiezhongyang <xiezhongyang@shengpay.com>
2022-06-16 15:40:44 +0800

    first commit

---
+++ src/test/java/com/seeseapay/gateway/api/GatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seeseapay.gateway.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
